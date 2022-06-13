8160101f0fdbcf82d3d6a3a9ca327df577d0e4ee
sa-sabuj <sasabuj321@gmail.com>
2022-06-13 20:43:57 +0600

    use hook

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,20 @@
+import './Register.css'
 import React from 'react';
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h2>Please login</h2>
-            <div style={{ margin: '15px' }}>
-                <button>Google Signin</button>
-            </div>
+            <h2>Please register</h2>
             <form>
+                <input type="text" name="" placeholder='Your name' />
+                <br />
                 <input type="email" name="" placeholder='Enter your email' /> <br />
                 <input type="password" name="" placeholder='Enter your password' />
                 <br />
-                <input type="submit" value="Login" />
+                <input type="submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
